1ea4100e3b6f77804247585b1bd2f7ea7f1a9eb6
malhotra-parul <parulm.business@gmail.com>
2020-05-01 18:18:22 +0530

    added a signout link in the header

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ class App extends Component {
   render(){
   return (
     <BrowserRouter>
-    <Header />
+    <Header currentUser={this.state.currentUser}/>
     <Switch>
       <Route exact path="/" component={HomePage} />
       <Route path="/shop" component={ShopPage} />
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,21 +1,34 @@
 import React from "react";
-import {Link} from "react-router-dom";
-import {ReactComponent as Logo} from "../../assets/original.svg";
+import { Link } from "react-router-dom";
+import { ReactComponent as Logo } from "../../assets/original.svg";
 import "./header.styles.scss";
+import { auth } from "../../firebase/firebase.utils";
 
-
-const Header = () => {
-    return (
-        <div className="header">
-        <Link className="logo" to="/">
-            <Logo />
+const Header = ({ currentUser }) => {
+  return (
+    <div className="header">
+      <Link className="logo" to="/">
+        <Logo />
+      </Link>
+      <div className="options">
+        <Link to="/shop" className="option">
+          SHOP
         </Link>
-        <div className="options">
-        <Link to="/shop" className="option">SHOP</Link>
-        <Link to="/contact" className="option">CONTACT US</Link>
-        <Link to="/signin" className="option">SIGN IN</Link>
-        </div>
-        </div>  );
-}
- 
+        <Link to="/contact" className="option">
+          CONTACT US
+        </Link>
+        {currentUser ? (
+          <div className="option" onClick={()=> auth.signOut()}>
+            SIGN OUT
+          </div>
+        ) : (
+          <Link to="/signin" className="option">
+            SIGN IN
+          </Link>
+        )}
+      </div>
+    </div>
+  );
+};
+
 export default Header;
